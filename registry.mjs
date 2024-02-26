bf238b2be0b4c5c2ab010c48c574cf232f7ee143
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2024-02-26 14:58:35 +0200

    refactor

--- registry.mjs
+++ registry.mjs
@@ -1,7 +1,7 @@
 export { Namespace } from 'component.namespace';
+export { RefId, Reference } from 'component.reference';
 export { Specs } from 'component.specs';
 export { Type } from 'component.type';
 export { GUID } from 'component.uuid';
-export { Reference } from 'component.reference';
 export { Property } from './lib/property.mjs';
 
--- specs/property-spec.mjs
+++ specs/property-spec.mjs
@@ -1,12 +1,21 @@
 import { Property } from '../registry.mjs';
-class FoodProperty extends Property {}
+class Food {
+    get kind() {
+        return Property.get({ kind: null }, String, Food);
+    }
+    set kind(value) {
+        Property.set({ kind: null }, String, Food, value);
+    }
+}
 describe('Property Specifiction Test: ', () => {
-    describe(`when creating a property given property name and type`, () => {
+    describe(`when creating a property given a property name and type, a target class and a value`, () => {
+        let food = null;
         beforeAll(() => {
-            new FoodProperty()
+            food = new Food();
+            food.kind = '5a101433-db49-49fe-89f0-49e204212a9e';
         });
-        it('should ', () => {
-        
+        it(`should return '5a101433-db49-49fe-89f0- 49e204212a9e'`, () => {
+            expect(food.kind).toBe('5a101433-db49-49fe-89f0-49e204212a9e');
         });
     });
 });
