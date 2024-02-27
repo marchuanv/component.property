01af495e7071baed554e7f40a9e82e965224f59c
marchuanv <marchuanv@gmail.com>
2024-02-27 03:09:49 +0200

    refactor

--- registry.mjs
+++ registry.mjs
@@ -1,8 +1,8 @@
-import { Type } from 'component.type';
+import { Type, TypeOptions } from 'component.type';
 export { Namespace } from 'component.namespace';
 export { RefId, Reference, ReferenceOptions } from 'component.reference';
 export { Specs } from 'component.specs';
 export { GUID } from 'component.uuid';
 export { Property } from './lib/property.mjs';
-export { Type };
+export { Type, TypeOptions };
 
