5d90e28da3a688d28ada34270472c5a340469221
marchuanv <marchuanv@gmail.com>
2024-03-17 09:18:57 +0200

    refactor

--- specs/property-spec.mjs
+++ specs/property-spec.mjs
@@ -1,57 +1,10 @@
 import { PropertiesReferenceContext, PropertiesTypeRegister, PropertyReference, PropertyTypeRegister, Reference, TypeRegister } from '../registry.mjs';
 import { Food } from './index.mjs';
-class FoodTypeRegister extends TypeRegister {
-    constructor() {
-        super(null, Food);
-    }
-}
-class StringTypeRegister extends TypeRegister {
-    constructor() {
-        super(null, String);
-    }
-}
-class BooleanTypeRegister extends TypeRegister {
-    constructor() {
-        super(null, Boolean);
-    }
-}
-let typeRegisters = [
-    new FoodTypeRegister(),
-    new StringTypeRegister(),
-    new BooleanTypeRegister()
+const properties = [
+    new PropertyTypeRegister(Food, 'name', String, true, true),
+    new PropertyTypeRegister(Food, 'isAdultFood', Boolean, true, true)
 ];
-typeRegisters = typeRegisters.map(register => {
-    const { Id, type, typeName } = register.get();
-    switch(type) {
-        case Food: {
-            const properties = typeRegisters.map(register => {
-                const { Id, type } = register.get();
-                switch(type) {
-                    case String: {
-                        return new PropertyTypeRegister(Id, 'name', type, true, true);
-                    }
-                    case Boolean: {
-                        return new PropertyTypeRegister(Id, 'isAdultFood', type, true, true);
-                    }
-                    default: {
-                        return null;
-                    }
-                }
-            }).filter(x => x);
-            return {
-                key: typeName,
-                value: new PropertiesTypeRegister(properties, Id)
-            };
-        }
-        default: {
-            return null;
-        }
-    }
-}).filter(x => x).reduce((obj, { key, value }) => {
-    obj[key] = value;
-    return obj;
-},{});
-const foodPropertiesTypeRegister = typeRegisters[Food.name];
+const foodPropertiesTypeRegister =  new PropertiesTypeRegister(properties, Food);
 describe('Property Specifiction Test: ', () => {
     describe(`when constructing the ${Food.name} class given default property options`, () => {
         it(`should not behave like a singleton`, () => {
