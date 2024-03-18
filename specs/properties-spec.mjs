8793958f86493b5570ce3ecf259b1a5b34474a91
marchuanv <marchuanv@gmail.com>
2024-03-19 00:49:26 +0200

    refactor

--- specs/properties-spec.mjs
+++ specs/properties-spec.mjs
@@ -25,19 +25,29 @@ describe('Properties Specifiction Test: ', () => {
             const dogA = new Dog(dogContextA);
             dogA.name = 'Luna';
             dogA.food = new Food(foodContextA);
+            dogA.food.name = 'dogmore';
+            dogA.food.isAdultFood = false;
 
             const dogB = new Dog(dogContextB);
             dogB.name = 'Bella';
             dogB.food = new Food(foodContextB);
+            dogB.food.name = 'epol';
+            dogB.food.isAdultFood = true;
 
             expect(dogA.name).toBe('Luna');
             expect(dogB.name).toBe('Bella');
+
+            expect(dogA.food.isAdultFood).toBeFalse();
+            expect(dogB.food.isAdultFood).toBeTrue();
+
+            expect(dogA.food.name).toBe('dogmore');
+            expect(dogB.food.name).toBe('epol');
         });
     });
     describe(`when constructing the ${Food.name} class given singleton reference context`, () => {
         it(`should behave like a singleton`, () => {
-            const dogContextA = new PropertiesReferenceContext(dogPropertiesTypeRegister);
-            const dogContextB = new PropertiesReferenceContext(dogPropertiesTypeRegister);
+            const dogContextA = new PropertiesReferenceContext(dogPropertiesTypeRegister, true);
+            const dogContextB = new PropertiesReferenceContext(dogPropertiesTypeRegister, true);
 
             const foodContextA = new PropertiesReferenceContext(foodPropertiesTypeRegister);
             const foodContextB = new PropertiesReferenceContext(foodPropertiesTypeRegister);
@@ -45,13 +55,23 @@ describe('Properties Specifiction Test: ', () => {
             const dogA = new Dog(dogContextA);
             dogA.name = 'Luna';
             dogA.food = new Food(foodContextA);
+            dogA.food.name = 'dogmore';
+            dogA.food.isAdultFood = false;
 
             const dogB = new Dog(dogContextB);
             dogB.name = 'Bella';
             dogB.food = new Food(foodContextB);
+            dogB.food.name = 'epol';
+            dogB.food.isAdultFood = true;
 
             expect(dogA.name).toBe('Bella');
             expect(dogB.name).toBe('Bella');
+
+            expect(dogA.food.isAdultFood).toBeTrue();
+            expect(dogB.food.isAdultFood).toBeTrue();
+
+            expect(dogA.food.name).toBe('epol');
+            expect(dogB.food.name).toBe('epol');
         });
     });
 });
