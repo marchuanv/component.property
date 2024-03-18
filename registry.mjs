44404c43a86bea80b550096f08752a58741566be
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2024-03-18 11:16:24 +0200

    refactor

--- registry.mjs
+++ registry.mjs
@@ -1,10 +1,10 @@
 export { Reference, ReferenceContext, ReferenceId } from 'component.reference';
 export { Specs } from 'component.specs';
-export { GUID, Store,TypeRegister,TypeRegisterId } from 'component.uuid';
+export { GUID, Store, Type, TypeRegister, TypeRegisterId } from 'component.uuid';
 export { Reflection } from 'utils';
 export { PropertiesReferenceContext } from './lib/properties-reference-context.mjs';
-export { PropertyReferenceContext } from './lib/property-reference-context.mjs';
-export { PropertiesTypeRegister } from './lib/properties-type-register.mjs';
 export { PropertiesReference } from './lib/properties-reference.mjs';
+export { PropertiesTypeRegister } from './lib/properties-type-register.mjs';
+export { PropertyReferenceContext } from './lib/property-reference-context.mjs';
 export { PropertyReference } from './lib/property-reference.mjs';
 export { PropertyTypeRegister } from './lib/property-type-register.mjs';
